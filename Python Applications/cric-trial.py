5864e426db8b3dc8b0b2eeb4dcd90622c6106b5f
Akshayan Sinha <akshayan.sinha@gmail.com>
2021-12-01 17:19:55 +0530

    updated trial code

--- Python Applications/cric-trial.py
+++ Python Applications/cric-trial.py
@@ -2,7 +2,6 @@ import json, requests, time
 from boltiot import Sms, Bolt
 import bolt_conf
 
-
 mybolt = Bolt(bolt_conf.API_KEY, bolt_conf.DEVICE_ID)
 
 base_url = "https://www.cricbuzz.com/api/cricket-match/commentary/"
@@ -10,9 +9,6 @@ base_url = "https://www.cricbuzz.com/api/cricket-match/commentary/"
 match = str(input("Enter Match ID: "))
 
 url = base_url + match
-
-batsman_six_0 = 0
-batsman_four_0 = 0
 
 while True:
 
@@ -22,17 +18,14 @@ while True:
     #print(line)
 
     batsman_name = str(line['miniscore']['batsmanNonStriker']['batName'])
-    batsman_six_1 = str(line['miniscore']['batsmanNonStriker']['batSixes'])
-    batsman_four_1 = str(line['miniscore']['batsmanNonStriker']['batFours'])
+    batsman_runs = int(line['miniscore']['batsmanNonStriker']['batRuns'])
+    
 
-
     print(".")
     print(".")
 
-    print("Hi !!"+ batsman_name, " has scored ", batsman_six_1, "sixes ..")
-    print(".")
-    print(".")
-    print("Hi !!"+ batsman_name, " has scored ", batsman_four_1, "fours ..")
+    print("Hi !! "+ batsman_name, " has scored ", batsman_runs, "runs ..")
+    
     mybolt.analogWrite(0,"150")
     time.sleep(0.2)
     mybolt.digitalWrite(0,"LOW")
